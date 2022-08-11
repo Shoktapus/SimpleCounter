6049b075cb6b3779e3185047d1f69713dfb4626d
Shoktapus <sinclair8075@gmail.com>
2022-08-11 21:41:42 +0000

    getting the math done

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,20 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Home from "./pages/home.jsx";
+let counter = 0;
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+setInterval(() => {
+    const six = counter/100000
+    const five = counter/10000
+    const four = counter/1000
+    const three = counter/100
+    const two = counter/10
+    const one = counter/1
+  ReactDOM.render(
+    <Home one={one} two={two} three={three} four={four} five={five} six={six} />,
+    document.querySelector("#app")
+  );
+  counter++
+}, 1000);
--- src/js/pages/home.jsx
+++ src/js/pages/home.jsx
@@ -4,21 +4,35 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+//easiest way to call props is to name the variable that your'e passing to the function
+//npm = node package manager
+//nvm = node version manager
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="counter">
+			<div className='clockIcon'>
+			<i className="fa-xl fa-regular fa-clock"></i>
+			</div>
+			<div className='digit'>
+				{props.six}
+			</div>
+			<div className='digit'>
+				{props.five}
+			</div>
+			<div className='digit'>
+				{props.four}
+			</div>
+			<div className='digit'>
+				{props.three}
+			</div>
+			<div className='digit'>
+				{props.two}
+			</div>
+			<div className='digit'>
+				{props.one}
+			</div>
+			
+	
 		</div>
 	);
 };
